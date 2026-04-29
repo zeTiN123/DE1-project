b0632ad74765e2262db2e4e284500200bb165c48
MFocher <269638@vut.cz>
2026-04-29 11:36:59 +0200

    Update tb_alarm_clock_top.vhd

--- Alarm_Clock/Alarm_Clock.srcs/sim_1/new/tb_alarm_clock_top.vhd
+++ Alarm_Clock/Alarm_Clock.srcs/sim_1/new/tb_alarm_clock_top.vhd
@@ -2,7 +2,6 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
 entity tb_alarm_clock_top is
--- Testbench nemá žádné porty
 end tb_alarm_clock_top;
 
 architecture tb of tb_alarm_clock_top is
@@ -60,9 +59,6 @@ begin
         wait;
     end process;
 
-    ------------------------------------------------------------------------
-    -- HLAVNÍ TESTOVACÍ SCÉNÁŘ
-    ------------------------------------------------------------------------
     stim_proc: process
         procedure push_button(signal btn : out std_logic) is
         begin
@@ -76,79 +72,52 @@ begin
         sw <= (others => '0');
         btnu <= '0'; btnc <= '0'; btnr <= '0'; btnd <= '0';
 
-        -- 1. ZAPNUTÍ DESKY A RESET (Zamčený komparátor)
         sw(15) <= '1'; wait for 2 us;
         sw(15) <= '0'; wait for 2 us;
 
-        --------------------------------------------------------------------
-        -- 2. NASTAVENÍ HLAVNÍHO ČASU NA 20:42
-        --------------------------------------------------------------------
-        sw(0) <= '1'; -- Zapneme režim HODIN
+        sw(0) <= '1'; 
         wait for 1 us;
 
-        -- Desítky hodin: 2
         for i in 1 to 2 loop push_button(btnu); end loop;
         push_button(btnr); 
 
-        -- Jednotky hodin: 0 (Tady jen posuneme řád dál)
         push_button(btnr); 
 
-        -- Desítky minut: 4
         for i in 1 to 4 loop push_button(btnu); end loop;
         push_button(btnr); 
 
-        -- Jednotky minut: 2
         for i in 1 to 2 loop push_button(btnu); end loop;
         push_button(btnr); 
 
-        --------------------------------------------------------------------
-        -- 3. NASTAVENÍ BUDÍKU NA 21:58
-        --------------------------------------------------------------------
-        sw(0) <= '0'; -- Zapneme režim BUDÍKU
+        sw(0) <= '0'; 
         wait for 1 us;
 
-        -- Desítky hodin: 2
         for i in 1 to 2 loop push_button(btnu); end loop;
         push_button(btnr); 
 
-        -- Jednotky hodin: 1
         push_button(btnu); 
         push_button(btnr); 
 
-        -- Desítky minut: 5
         for i in 1 to 5 loop push_button(btnu); end loop;
         push_button(btnr); 
 
-        -- Jednotky minut: 8
         for i in 1 to 8 loop push_button(btnu); end loop;
         push_button(btnr);
 
-        --------------------------------------------------------------------
-        -- 4. SPUŠTĚNÍ SYSTÉMU A ČEKÁNÍ NA SHODU
-        --------------------------------------------------------------------
-        sw(1) <= '1'; -- Zapnutí budíku (připraveno na zítřek)
+        sw(1) <= '1'; 
         wait for 1 us;
         
-        sw(0) <= '1'; -- Návrat zobrazení zpět na hodiny
+        sw(0) <= '1'; 
         wait for 1 us;
         
-        -- Zmáčkneme BTNC = Start hodin a Odemčení komparátoru
         push_button(btnc);
 
-        -- Nyní čekáme 1 milisekundu (V simulačním čase to odpovídá cca 83 minutám).
-        -- Během této doby čas přeteče přes 21:58 a budík začne zvonit (blikat led16_r).
         wait for 1 ms;
 
-        --------------------------------------------------------------------
-        -- 5. VYPNUTÍ ZVONÍCÍHO BUDÍKU
-        --------------------------------------------------------------------
-        -- Dalším stiskem BTNC budík "zamáčkneme" a umlčíme ho.
         push_button(btnc);
         
-        -- Chvilku počkáme, abychom v grafu viděli, že led16_r opravdu zhasla
         wait for 10 us;
 
-        -- 6. UKONČENÍ TESTOVÁNÍ
         sim_ended <= true;
         wait;
     end process;
